bb8489885e88ad10c18df91c82d73512b555023a
pk12002 <cefoxay132@loongwin.com>
2023-12-20 12:25:45 +0530

    added comments and json

--- MOD.py
+++ MOD.py
@@ -2,35 +2,38 @@ import time
 import json
 from pyModbusTCP.client import ModbusClient
 
-
+# Establishing Modbus client and loading addresses from a JSON file
 client = ModbusClient(host="192.168.1.3", port=502)
-addresses = [(0, 1), (2, 0), (3, 1), (5, 0), (6, 0), (7, 0), (8, 1)]
+f = open('addresses.json')
+addresses = json.load(f)["addresses"]
 
-
+# Function to read data based on the address and data type
 def read_data(address, data_type):
-    if data_type == 1:  
+    if data_type == 1:  # For double registers
         data = client.read_holding_registers(address, 2)
         if data:
             counter, overflow = data
             double = (overflow * 65536) + counter
             return double
-    elif data_type == 0:  
+    elif data_type == 0:  # For single registers
         data = client.read_holding_registers(address, 1)
         if data:
             return data[0]
     return None
 
-
+# Check if client is connected to the Modbus server
 connected = client.open()
 
+# Continuously fetch and print data
 while True:    
-    if not connected:
+    if not connected:  # Reconnect if not connected
         print("CHECK LAN")
         connected = client.open()
         time.sleep(2)
         continue
 
     output_data = []
+    # Go through addresses and read data
     for addr, data_type in addresses:
         result = read_data(addr, data_type)
         if result:
@@ -40,8 +43,9 @@ while True:
                 "data": result
             })
 
+    # Print collected data as JSON
     for data_point in output_data:
         json_output = json.dumps(data_point)
         print(json_output)
 
-    time.sleep(5)
+    time.sleep(5)  # Wait for 5 seconds before fetching data again
